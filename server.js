7d3da59a4a57f9e917cc7f508f6c642eac0a69c4
inmarluna <inmarluna@Inmars-MacBook-Pro.local>
2022-01-04 23:08:17 -0600

    file setup and server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+    useFindAndModify: false,
+    // userNewUrlParser: true,
+    useUnifiedTopology: true
+});
+// use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
